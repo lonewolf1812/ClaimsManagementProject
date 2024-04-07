8ecdfbfb6445feb1917170e5e9d05466a7f0ad4b
Asus <Asus@DESKTOP-13TPCES>
2024-04-07 12:38:50 +0530

    git commit

---
+++ ClaimsEurekaServer/src/test/java/com/claimsmanagement/ClaimsEurekaServer/ClaimsEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claimsmanagement.ClaimsEurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimsEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
